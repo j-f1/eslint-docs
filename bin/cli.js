1f5bfa713880508acae368f1d77c9d069053b7e6
Jed Fox <git@twopointzero.us>
2017-08-20 13:38:08 -0400

    Add the code

---
+++ bin/cli.js
@@ -0,0 +1,13 @@
+const chalk = require('chalk')
+
+const { Abort, unabort } = require('../lib/abort')
+const spinner = require('../lib/spinner')
+
+const run = require('../lib')
+
+unabort(run)().catch(err => {
+  if (!(err instanceof Abort)) {
+    spinner.fail('Oops, something went wrong!')
+    console.error(chalk.gray(err.stack))
+  }
+})
