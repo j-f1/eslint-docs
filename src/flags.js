72a7638218eee38148da346c7e4b15a179b9e4e4
Jed Fox <git@twopointzero.us>
2017-08-21 13:50:15 -0400

    Add a function to parse the flags separately

--- src/flags.js
+++ src/flags.js
@@ -1,25 +1,30 @@
-const args = process.argv.slice(2)
+const argv = process.argv.slice(2)
 
-const isChecking = args.includes('check')
-const verb = exports.isChecking ? 'Checking' : 'Updating'
+const parse = (args = argv) => {
+  const isChecking = args.includes('check')
+  const verb = exports.isChecking ? 'Checking' : 'Updating'
 
-const extArg = args.find(arg => arg.startsWith('--ext'))
-let ext = '.js'
-if (extArg) {
-  if (extArg.startsWith('--ext=')) {
-    ext = extArg.replace(/^--ext=/, '')
-  } else if (extArg === '--ext' && args.indexOf(extArg) < args.length - 1) {
-    ext = args[args.indexOf(extArg) + 1]
+  const extArg = args.find(arg => arg.startsWith('--ext'))
+  let ext = '.js'
+  if (extArg) {
+    if (extArg.startsWith('--ext=')) {
+      ext = extArg.replace(/^--ext=/, '')
+    } else if (extArg === '--ext' && args.indexOf(extArg) < args.length - 1) {
+      ext = args[args.indexOf(extArg) + 1]
+    }
+  }
+
+  if (!ext.match(/\..+$/)) {
+    ext = `.${ext}`
+  }
+
+  return {
+    args,
+    isChecking,
+    verb,
+    ext,
+    parse,
   }
 }
 
-if (!ext.match(/\..+$/)) {
-  ext = `.${ext}`
-}
-
-exports = module.exports = {
-  args,
-  isChecking,
-  verb,
-  ext,
-}
+exports = module.exports = parse()
