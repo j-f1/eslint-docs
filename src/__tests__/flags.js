daaf7a34be86ec88e9e7980870f3ba56d027df5e
Jed Fox <git@twopointzero.us>
2017-08-21 13:50:27 -0400

    Test flags.js

---
+++ src/__tests__/flags.js
@@ -0,0 +1,49 @@
+const { parse } = require('../flags')
+
+const checkArgs = args => expect(parse(args)).toMatchSnapshot()
+const dropArgs = parsed => {
+  delete parsed.args
+  return parsed
+}
+
+describe('parse()', () => {
+  it('returns the full object', () => {
+    checkArgs([])
+  })
+
+  it('supports `check`', () => {
+    checkArgs(['check'])
+  })
+
+  it('supports --ext .foo', () => {
+    checkArgs(['--ext', '.ts'])
+  })
+  it('supports --ext=.foo', () => {
+    checkArgs(['--ext=.ts'])
+  })
+
+  it('supports --ext foo', () => {
+    checkArgs(['--ext', 'ts'])
+  })
+  it('supports --ext=foo', () => {
+    checkArgs(['--ext=ts'])
+  })
+
+  it('supports both --ext and check', () => {
+    checkArgs(['--ext', 'js', 'check'])
+    expect(dropArgs(parse(['--ext', 'js', 'check']))).toEqual(
+      dropArgs(parse(['check', '--ext', 'js']))
+    )
+  })
+  it('supports both --ext=foo and check', () => {
+    checkArgs(['--ext=js', 'check'])
+    expect(dropArgs(parse(['--ext=js', 'check']))).toEqual(
+      dropArgs(parse(['check', '--ext=js']))
+    )
+  })
+
+  it('ignores --ext at the end of the args', () => {
+    checkArgs(['--ext'])
+    checkArgs(['check', '--ext'])
+  })
+})
