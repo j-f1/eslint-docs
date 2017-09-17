0dfbf6cea6b27de90296ca5e3c7b9e122144918a
Jed Fox <git@twopointzero.us>
2017-09-17 15:10:04 -0400

    Use a sentinel instead of an Error subclass when aborting
    It caused errors for some reason.

--- src/abort.js
+++ src/abort.js
@@ -2,17 +2,17 @@ exports = module.exports = function abort() {
   if (!process.exitCode) {
     process.exitCode = 1
   }
-  throw new exports.Abort()
+  throw exports.Abort
 }
 
-exports.Abort = class extends Error {}
+exports.Abort = {}
 
 exports.unabort = f => async (...args) => {
   try {
     return await f(...args)
   } catch (e) {
     // silently catch the abort() call
-    if (!(e instanceof exports.Abort)) {
+    if (e !== exports.Abort) {
       throw e
     }
   }
