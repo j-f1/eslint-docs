f835d340056c2b65a814d37eff2e283029ef8a34
Jed Fox <git@twopointzero.us>
2017-09-17 15:49:44 -0400

    Use unabort() in the CLI for error handling

--- src/cli.js
+++ src/cli.js
@@ -2,16 +2,14 @@
 
 const chalk = require('chalk')
 
-const { Abort } = require('../lib/abort')
+const { unabort } = require('../lib/abort')
 const spinner = require('../lib/spinner')
 
 const run = require('../lib')
 
-run()
+unabort(run)()
   .catch(err => {
-    if (!(err instanceof Abort)) {
-      spinner.fail('Oops, something went wrong!')
-      console.error(chalk.gray(err.stack))
-    }
+    spinner.fail('Oops, something went wrong!')
+    console.error(chalk.gray(err.stack || err.toString()))
   })
   .then(() => console.log(''))
